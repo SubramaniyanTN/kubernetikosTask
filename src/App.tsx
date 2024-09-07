7b8293606dba1d9fd4cafbbe838665c9e66965ea
subramaniyan <subramaniyanm@xbi4.com>
2024-09-07 08:29:36 +0530

    integrated mantine ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./Router/Router";
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
 
+import { createTheme, MantineProvider } from "@mantine/core";
 function App() {
-  return <RouterProvider router={router} />;
+  const theme = createTheme({});
+  return (
+    <MantineProvider theme={theme}>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  );
 }
 
 export default App;
